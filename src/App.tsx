db6c33ce7ce3cb79d28b1eaa9c00321ba87e215f
X Misfit <akoushik88@gmail.com>
2024-08-21 23:07:55 +0530

    add: initial files

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import './App.css';
+import linkedinIcon from './assets/linkedin.png';
+import githubIcon from './assets/github.png';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <a href="www.linkedin.com/in/srikousik" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
+        </a>
+        <a href="https://github.com/MisfiT2020" target="_blank" rel="noopener noreferrer">
+          <img src={githubIcon} alt="GitHub" className="social-icon" />
+        </a>
+      </header>
+      <h1>I'm Kousik</h1>
+      <div className="animated-text">
+        <Typewriter
+          options={{
+            strings: ["I'm a coder", "I'm a developer", "I'm a designer", "I'm a Telegram Developer"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
